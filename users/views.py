36b2b44db358beb8327acb155798ec6218657e4c
PrimChim <85182130+PrimChim@users.noreply.github.com>
2023-12-29 07:39:33 +0545

    finishing to register api

--- users/views.py
+++ users/views.py
@@ -1,16 +1,9 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.shortcuts import redirect
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 
-from rest_framework import viewsets, permissions, generics, status
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view
-from rest_framework import serializers
-from .serializers import UserSerializer, UserRegisterSerializer, UserLoginSerializer, UserUpdateSerializer, UserChangePasswordSerializer, UserDeleteSerializer
 
 #register api
 @api_view(['POST'])
@@ -31,14 +24,9 @@ def register(request):
             # Create a new user
             user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
             print(user)
-            # Save the user
             user.save()
-            # Redirect to the register page with a success message
             return Response({'success': 'User Created Successfully'}, status=status.HTTP_201_CREATED)
-        # Otherwise, the passwords do not match
         else:
-            # Redirect to the registration page with an error message
             return Response({'error': 'Passwords do not match'}, status=status.HTTP_400_BAD_REQUEST)
-    # Otherwise, the user is trying to access the registration page
     else:
         return Response({'error': 'Invalid Request'}, status=status.HTTP_400_BAD_REQUEST)
