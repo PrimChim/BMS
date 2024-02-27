49c3a8d4a540ab13cd4b06c0c1d3e049133cd283
PrimChim <gpritam922@gmail.com>
2024-02-27 15:17:43 +0545

    customer search
    HTML/CSS/JS for customer search almost complete

--- billing/static/billing/js/create-bill.js
+++ billing/static/billing/js/create-bill.js
@@ -10,20 +10,73 @@ function fetchCustomers() {
 }
 
 let customerPan = document.querySelectorAll('.customer-details')[1];
+
+let customersList = document.getElementById('customers-list');
 if (localStorage.getItem('customers') === null) {
     fetchCustomers();
 } else {
     let customers = JSON.parse(localStorage.getItem('customers'));
-    let customerSelect = document.getElementById('customer');
-    customers.forEach(customer => {
-        let option = document.createElement('option');
-        option.value = customer.pan;
-        option.text = customer.name;
-        customerSelect.appendChild(option);
-    });
-    customerPan.value = customers[0].pan;
-
+    if (customers != null) {
+        customers.forEach(customer => {
+            customersList.innerHTML += `<li onClick="setCustomer('${customer.name}', ${customer.pan})">${customer.name}</li>`;
+            console.log(customer);
+        });
+    }
 }
+
+let customer = document.getElementById('customer');
+
+function setCustomer(name, pan) {
+    customer.value = name;
+    customerPan.value = pan;
+    customersList.style.display = 'none';
+}
+
+customer.addEventListener('focus', function () {
+    if (this.value == '') {
+        customersList.style.display = 'block';
+    }
+
+    let customers = JSON.parse(localStorage.getItem('customers'));
+    let filteredCustomers = customers.filter(customer => customer.name.toLowerCase().includes(this.value.toLowerCase()));
+
+    customersList.innerHTML = '';
+    filteredCustomers.forEach(customer => {
+        customersList.innerHTML += `<li onClick="setCustomer('${customer.name}', ${customer.pan})">${customer.name}</li>`;
+    });
+    customersList.style.display = 'block';
+
+    if (filteredCustomers.length == 0) {
+        fetchCustomers();
+        customersList.style.display = 'none';
+    }
+})
+
+customer.addEventListener('input', function () {
+    customersList.style.display = 'block';
+
+    let customers = JSON.parse(localStorage.getItem('customers'));
+    let filteredCustomers = customers.filter(customer => customer.name.toLowerCase().includes(this.value.toLowerCase()));
+    
+    if (filteredCustomers.length == 0) {
+        fetchCustomers();
+        customersList.style.display = 'none';
+    }
+    
+    customersList.innerHTML = '';
+    filteredCustomers.forEach(customer => {
+        customersList.innerHTML += `<li onClick="setCustomer('${customer.name}', ${customer.pan})">${customer.name}</li>`;
+    });
+});
+
+customer.addEventListener('focusout', function () {
+    if (this.value == '') {
+        customerPan.value = '';
+    }
+    setTimeout(() => {
+        customersList.style.display = 'none';
+    }, 200);
+})
 
 document.getElementById('customer').addEventListener('change', function () {
     let customers = JSON.parse(localStorage.getItem('customers'));
