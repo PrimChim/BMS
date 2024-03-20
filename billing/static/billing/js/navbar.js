16aa1d41c4300384323b32bd6efb439b8c4c30ac
PrimChim <gpritam922@gmail.com>
2024-03-20 15:16:19 +0545

    navbar functions added

--- billing/static/billing/js/navbar.js
+++ billing/static/billing/js/navbar.js
@@ -1,6 +1,8 @@
 let uri = window.location.pathname;
 const createBills = document.getElementById('create-bill');
 const viewBills = document.getElementById('view-bills');
+const button = document.getElementById('burger');
+const nav = document.getElementById('nav');
 
 switch(uri) {
     case '/billing/create-bill/':
@@ -10,3 +12,24 @@ switch(uri) {
         viewBills.classList.add('active')
         break
 };
+
+function changeIcon(){
+    if (button.classList.contains('closed')){
+
+        button.classList.remove('closed');
+        button.classList.add('open');
+
+        button.innerHTML = '&times';
+        button.style.font = '40px';
+
+        nav.classList.remove('collapsed');
+    }else{
+        
+        button.classList.remove('open');
+        button.classList.add('closed');
+
+        button.innerHTML = '&#9776;';
+
+        nav.classList.add('collapsed');
+    }
+}
